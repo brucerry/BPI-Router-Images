7becaf1545d8cc0f7f901766efa1b95e97f22196
bruce cheung <bruce.cheung@emplustech.com>
2026-01-02 16:48:34 +0800

    run.sh: Support building model bpi-r4lite & linux 6.18.0

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 function usage {
     echo "Usage: $0 <model> <distro>"
-    echo "Available models: bpi-r3 bpi-r4"
+    echo "Available models: bpi-r3 bpi-r4 bpi-r4lite"
     echo "Available distro:"
     echo "    * Debian: buster bullseye bookworm trixie"
     echo "    * Ubuntu: focal jammy noble"
@@ -26,14 +26,19 @@ done
 
 [ -z "$name" ] && echo "Unsupported distro: $distro" && exit 1
 
+variant="none"
+
 case $model in
     bpi-r3) ;;
     bpi-r4) ;;
+    bpi-r4lite)
+        variant=bpi-r4lite
+        ;;
     *) echo "Unsupported model: $model" && usage && exit 1 ;;
 esac
 
 uboot=${model}_emmc.img.gz
-kernel=${model}_6.17.0-main.tar.gz
+kernel=${model}_6.18.0-main.tar.gz
 
 [ ! -e $uboot ] && echo "File not found: $uboot" && exit 1
 [ ! -e $kernel ] && echo "File not found: $kernel" && exit 1
@@ -43,13 +48,20 @@ time {
     sudo umount -l ${name}_${distro}_arm64
     sudo rm -rf ${name}_${distro}_arm64 ${distro}_arm64.tar.gz
     git restore .
+    [ $model = "bpi-r4lite" ] && model=bpi-r4
     conffile=sourcefiles_${model}.conf
     rm -rf $conffile
+    [ $variant = "bpi-r4lite" ] && echo "variant=$variant" >> $conffile
     [ $model = "bpi-r4" ] && echo -e "replacehostapd=1\nreplaceiperf=1" >> $conffile
     echo "skipubootdownload=1" >> $conffile
     echo "skipkerneldownload=1" >> $conffile
     echo "imgfile=$uboot" >> $conffile
     echo "kernelfile=$kernel" >> $conffile
     echo "userpackages=\"ethtool iperf3 tcpdump vim git tig mtd-utils memtester file pciutils usbutils traceroute net-tools psmisc wget curl fdisk ack bridge-utils wpasupplicant isc-dhcp-client man tshark mmc-utils\"" >> $conffile
+    "########################################################"
+    "$conffile:"
+    ""
+    cat $conffile
+    "########################################################"
     ./buildimg.sh $model $distro
 }
